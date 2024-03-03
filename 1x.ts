db03f0577a76491bd80fc251cdb87665cd1b82c3
Alexey Trofimenko <aronaxis@gmail.com>
2024-03-04 01:24:35 +0400

    Use indexOf

--- 1x.ts
+++ 1x.ts
@@ -1,37 +1,32 @@
 import * as fs from 'fs';
 
-function reverseString(str: string): string {
-    return str.split('').reverse().join('');
+const digits: string[] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+const digitsMapping: Record<string, number> = {}
+
+Object.entries(digits).forEach(([index, digit]) => digitsMapping[digit] = parseInt(index) + 1 );
+for (let i = 1; i <= 9; i++) { digitsMapping[i.toString()] = i }
+
+function firstDigit(line: string): number {
+    return Object.entries(digitsMapping)
+        .map(([substr, value]) => [line.indexOf(substr), value])
+        .filter(([index, value]) => index !== -1)
+        .reduce((min, [index, value]) => (index < min[0] ? [index, value] : min))[1];
 }
 
-const filePath = process.argv[2];
-
-const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-const reverseNumbers = numbers.map(reverseString);
-const matcher = new RegExp("\\d|" + numbers.join("|"));
-const reverseMatcher = new RegExp("\\d|" + reverseNumbers.join("|"));
-
-const numbersMapping: Record<string, number> = numbers.reduce((obj: Record<string, number>, num, index) => {
-    obj[num] = index;
-    return obj;
-}, {});
-
-for (let i = 0; i <= 9; i++) { numbersMapping[i.toString()] = i }
+function lastDigit(line: string): number {
+    return Object.entries(digitsMapping)
+        .map(([substr, value]) => [line.lastIndexOf(substr), value])
+        .filter(([index, value]) => index !== -1)
+        .reduce((max, [index, value]) => (index > max[0] ? [index, value] : max))[1];
+}
 
 let sum = 0;
 
-function dbg(value:any) {
-    console.log(value);
-    return value
-}
-
-fs.promises.readFile(filePath, 'utf-8')
+fs.promises.readFile(process.argv[2], 'utf-8')
     .then((content) => content.split("\n").slice(0, -1))
     .then((lines) => lines.forEach((line: string) => {
-        let firstDigit = numbersMapping[line.match(matcher)![0]];
-        let lastDigit = numbersMapping[reverseString(reverseString(line).match(reverseMatcher)![0])];
-        let inc = firstDigit * 10 + lastDigit;
-        sum += inc
+        sum += firstDigit(line) * 10 + lastDigit(line);
     }))
     .then(() => console.log(sum))
     .catch(console.error);
