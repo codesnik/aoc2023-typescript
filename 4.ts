2d1c608a2bc563080a735ece15a7337c07b0f689
Alexey Trofimenko <aronaxis@gmail.com>
2024-03-07 04:09:31 +0400

    Day 4

---
+++ 4.ts
@@ -0,0 +1,42 @@
+import * as fs from 'fs';
+import _ from 'lodash';
+
+/**
+    parseLine("Card 34:  1 12 35 | 1 5 6") // => [34, [1, 12, 35], [1, 5, 6]]
+*/
+function parseLine(line: string): [number, number[], number[]] {
+  const [card, rest] = line.split(":");
+  const cardNum = parseInt(card.split(/\s+/)[1]);
+  const [winning, gotten] = rest.split(" | ").map(str => str.trim().split(/\s+/).map(n => parseInt(n)));
+  return [cardNum, winning, gotten]
+}
+
+/** doubles points for each additional counted item
+    points(0) // => 0
+    points(1) // => 1
+    points(3) // => 4
+*/
+function points(count: number): number {
+  if (count==0) {
+    return 0;
+  }
+  let score = 1;
+  for (let i=count; i > 1; i--) {
+    score = score * 2
+  }
+  return score
+}
+
+function linePoints(line: string): number {
+  const [cardNum, winning, gotten] = parseLine(line);
+  const count = gotten.filter(num => winning.includes(num)).length;
+  return points(count)
+}
+
+fs.promises.readFile(process.argv[2], 'utf-8')
+  .then((content) => content.split("\n").slice(0, -1))
+  .then((lines) =>
+    lines.map(linePoints).reduce((sum, n) => sum + n, 0)
+  )
+  .then(console.log)
+  .catch(console.error)
---
+++ 4x.ts
@@ -0,0 +1,29 @@
+import * as fs from 'fs';
+import _ from 'lodash';
+
+/**
+    parseLine("Card 34:  1 12 35 | 1 5 6") // => [34, 1]
+*/
+function parseLine(line: string): [number, number] {
+  const [card, rest] = line.split(":");
+  const cardNum = parseInt(card.split(/\s+/)[1]);
+  const [winning, gotten] = rest.split(" | ").map(str => str.trim().split(/\s+/).map(n => parseInt(n)));
+  const wins = gotten.filter(num => winning.includes(num)).length;
+  return [cardNum, wins]
+}
+
+fs.promises.readFile(process.argv[2], 'utf-8')
+  .then((content) => content.split("\n").slice(0, -1))
+  .then((lines) => {
+    let cardWins: number[] = [];
+    lines.map(parseLine).forEach(([cardNum, wins]) => cardWins[cardNum - 1] = wins);
+    let cardCounts: number[] = Array(cardWins.length).fill(1);
+    for (let i = 0; i < cardWins.length; i++) {
+      for (let j = i+1; j <= i + cardWins[i]; j++ ) {
+        cardCounts[j] += cardCounts[i]
+      }
+    }
+    return cardCounts.reduce((sum, n) => sum + n)
+  })
+  .then(console.log)
+  .catch(console.error)
